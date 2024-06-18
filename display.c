1f1769b824267840a5da6fd0f29e0a0a2fce1fb0
Dulith Jayasekara <it23409828@my.sliit.lk>
2024-06-18 21:59:03 +0530

    Final Code
    This is the final code of the program. Consists of 1 header file, 4 source files containing the functions and 1 main program.

--- display.c
+++ display.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
+// Importing the global variable from main program
+extern int matrix[5][5];
+
 void displayWelcome()
 {
-    puts("This is game in which you have to place 5 obstacles within a 5 by 5 matrix and move through it.");
-    puts("Give valid (x,y) coordinates for the obstacles.");
-    puts("Eg: 3 4 - here 3 is the x coordinate and 4 is the y coordinate.");
+  puts("This is game in which you have to place 5 obstacles within a 5 by 5 matrix and move through it.");
+  puts("Give valid (x,y) coordinates for the obstacles.");
+  puts("Eg: 3 4 - here 3 is the x-coordinate and 4 is the y-coordinate.\n");
 }
 
-void displayMatrix()
+void displayMatrix() 
 {
-    int i, j;
+  int i, j;
+  puts("");
 
-    for(i = 0; i < 5; i++)
+  for (i = 0; i < 5; i++) 
+  {
+    for (j = 0; j < 5; j++) 
     {
-        for(j = 0; j < 5; j++)
-        {
-            printf("%2d",matrix[i][j]);
-        }
-        puts();
+      printf("%2d", matrix[i][j]);
     }
+    puts("");
+  }
+
+  puts("");
 }
-    
